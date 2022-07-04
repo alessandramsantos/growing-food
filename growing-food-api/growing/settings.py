78972d6b129297cea04787ce61bc7fa6d3f4c3fc
Alessandra Plets <itsalematos@gmail.com>
2022-07-04 11:42:37 -0400

    Feat: updating django version and allowing django cors

--- growing-food-api/growing/settings.py
+++ growing-food-api/growing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ipin$2xixi+u8-b41)8j1njw)*(!ju(g(l$_5x-xco1v4v14*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'food.apps.FoodConfig',
+    "corsheaders",
     'rest_framework',
+    'food.apps.FoodConfig',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://localhost:3000",
+#     "http://localhost:8000",
+# ]
+
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000',
+  'http://localhost:3000',
+)
